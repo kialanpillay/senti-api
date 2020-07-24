0adc0b2871ec28c173667999c301b438a17895c2
Kialan Pillay <kialanpillay@gmail.com>
2020-07-24 18:49:57 +0200

    Docstrings

--- app/naive_bayes.py
+++ app/naive_bayes.py
@@ -10,7 +10,17 @@ from nltk import FreqDist, classify, NaiveBayesClassifier
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 def remove_noise(tweet_tokens, stop_words=()):
-
+    """
+    Removes noise from a list of text tokens using regular expressions
+    and lemmatization
+    Parameters
+    ----------
+    tokens (list): Text tokens
+    stop_words (tuple): stopwords
+    Returns
+    -------
+    List
+    """
     cleaned_tokens = []
 
     for token, tag in pos_tag(tweet_tokens):
@@ -21,17 +31,18 @@ def remove_noise(tweet_tokens, stop_words=()):
             token,
         )
         token = re.sub("(@[A-Za-z0-9_]+)", "", token)
-
+        #Part-of-speech
         if tag.startswith("NN"):
             pos = "n"
         elif tag.startswith("VB"):
             pos = "v"
         else:
             pos = "a"
-
+        #Token lemmatization
         lemmatizer = WordNetLemmatizer()
         token = lemmatizer.lemmatize(token, pos)
 
+        #Token cleaning
         if (
                 len(token) > 0
                 and token not in string.punctuation
@@ -42,36 +53,72 @@ def remove_noise(tweet_tokens, stop_words=()):
 
 
 def get_all_words(cleaned_tokens_list):
+    """
+    Generates a list of words from all tweet tokens.
+    Parameters
+    ----------
+    cleaned_tokens_list (list): Cleaned tweet tokens
+    Returns
+    -------
+    Generator
+    """
     for tokens in cleaned_tokens_list:
         for token in tokens:
+            #Generator function
             yield token
 
 
 def get_tweets_for_model(cleaned_tokens_list):
+    """
+    Generates a dictionary from cleaned tokens.
+    Parameters
+    ----------
+    cleaned_tokens_list (list): Cleaned tweet tokens
+    Returns
+    -------
+    Generator
+    """
     for tweet_tokens in cleaned_tokens_list:
+        #Generator function
         yield dict([token, True] for token in tweet_tokens)
 
 
 def train_model():
+    """
+    Trains a Naive Bayes sentiment classifier using the twitter_samples
+    dataset from NLTK. Each tweet is tokenized and cleaned to produce a training
+    dataset for the machine learning model.
+    Parameters
+    ----------
+    Returns
+    -------
+    NaiveBayesClassifier
+    """
+    #Load dataset from nltk data
     positive_tweets = twitter_samples.strings("positive_tweets.json")
     negative_tweets = twitter_samples.strings("negative_tweets.json")
 
+    #Retrieve english stop words
     stop_words = stopwords.words("english")
 
+    #Tweet tokenization
     positive_tweet_tokens = twitter_samples.tokenized("positive_tweets.json")
     negative_tweet_tokens = twitter_samples.tokenized("negative_tweets.json")
 
     positive_cleaned_tokens_list = []
     negative_cleaned_tokens_list = []
 
+    #Token cleaning
     for tokens in positive_tweet_tokens:
         positive_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
 
     for tokens in negative_tweet_tokens:
         negative_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
 
+    #Extract words from tokens
     all_pos_words = get_all_words(positive_cleaned_tokens_list)
 
+    #Frequency distribition of words
     freq_dist_pos = FreqDist(all_pos_words)
 
     positive_tokens_for_model = get_tweets_for_model(
@@ -79,6 +126,7 @@ def train_model():
     negative_tokens_for_model = get_tweets_for_model(
         negative_cleaned_tokens_list)
 
+    #Create datasets
     positive_dataset = [
         (tweet_dict, "Positive") for tweet_dict in positive_tokens_for_model
     ]
@@ -86,7 +134,7 @@ def train_model():
     negative_dataset = [
         (tweet_dict, "Negative") for tweet_dict in negative_tokens_for_model
     ]
-
+    #Merge individual datasets into singular training data
     dataset = positive_dataset + negative_dataset
 
     train_data = dataset
@@ -95,6 +143,9 @@ def train_model():
     return classifier
 
 def main():
+    """
+    Runs training for the Naive Bayes classifier and pickles the trained model
+    """
     classifier = train_model()
     save_classifier = open("naivebayes.pickle","wb")
     pickle.dump(classifier, save_classifier)
--- app/senti.py
+++ app/senti.py
@@ -17,9 +17,28 @@ from nltk.tokenize import word_tokenize
 from nltk import FreqDist, classify, NaiveBayesClassifier
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
+"""
+Senti API for Sentiment Analysis built with Flask and NLTK for the NLP.
+Provides endpoints for analysis, bulk sentiment analysis, and open-source
+corpus contributions.
+
+Classes:
+    Item
+    Corpus
+    Vader
+    Bulk
+"""
+
+# Create Flask application
 app = Flask(__name__)
 api = Api(app=app, version="1.0", title="Senti API")
 
+#Logging Config
+logging.basicConfig(format='%(asctime)s - [%(levelname)s] %(message)s',
+                        datefmt='%d-%b-%y %H:%M:%S',
+                        level=logging.INFO)
+
+# API Models
 get_request = api.model(
     "GET Request", {"text": fields.String(
         required=True, description="Text to classify")}
@@ -39,14 +58,35 @@ put_request = api.model('PUT Request', {
 })
 
 class Item(fields.Raw):
+    """
+    A class that represents a POST request payload.
+    ...
+    Attributes
+    ----------
+    value: dict
+        request object
+    Methods
+    -------
+    format(self, value):
+        Returns a dictionary of attributes.
+    """
     def format(self, value):
         return {'user': value.user, 'phrase': value.phrase, 'sentiment': value.sentiment}
 
-def remove_noise(tweet_tokens, stop_words=()):
-
+def remove_noise(tokens, stop_words=()):
+    """
+    Removes noise from a list of text tokens using regular expressions
+    and lemmatization.
+    Parameters
+    ----------
+    tokens (list): Text tokens
+    stop_words (tuple): stopwords
+    Returns
+    -------
+    List
+    """
     cleaned_tokens = []
-
-    for token, tag in pos_tag(tweet_tokens):
+    for token, tag in pos_tag(tokens):
         token = re.sub(
             "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+#]|[!*\(\),]|"
             "(?:%[0-9a-fA-F][0-9a-fA-F]))+",
@@ -54,7 +94,7 @@ def remove_noise(tweet_tokens, stop_words=()):
             token,
         )
         token = re.sub("(@[A-Za-z0-9_]+)", "", token)
-
+        # Part-of-speech
         if tag.startswith("NN"):
             pos = "n"
         elif tag.startswith("VB"):
@@ -74,19 +114,41 @@ def remove_noise(tweet_tokens, stop_words=()):
     return cleaned_tokens
 
 def naive_bayes(text, classifier):
-
-    tokens = []
-    stop_words = stopwords.words("english")
-
+    """
+    Classifies text using a pre-trained Naive Bayes classifier and returns
+    a sentiment classification.
+    Parameters
+    ----------
+    text (String): Text to classify
+    classifier (Object): Trained naive bayes classifier
+    Returns
+    -------
+    String
+    """
+    #Token cleaning
     text_tokens = remove_noise(word_tokenize(text))
     classification = classifier.classify(
         dict([token, True] for token in text_tokens)
     )
     return classification
 
-def vader(text, classifier):
+def vader(text):
+    """
+    Classifies text using a SentimentIntensityAnalyzer trained using the
+    VADER algorithm and returns a sentiment classification and polarity scores.
+    Parameters
+    ----------
+    text (String): Text to classify
+    classifier (Object): Trained naive bayes classifier
+    Returns
+    -------
+    dict
+    """
+    #Create SentimentIntensityAnalyzer (using VADER) object
     sid = SentimentIntensityAnalyzer()
+    #Analyse text
     score = sid.polarity_scores(text)
+    # Convert float value to String for client-side rendering
     if score["compound"] > 0.05 :
         score["classification"] = "Positive"
     elif score["compound"] < -0.05:
@@ -96,13 +158,23 @@ def vader(text, classifier):
     return score
 
 def insert(item):
+    """
+    Inserts and open-source Gen Z corpus submission into a DynamoDB table instance.
+    Parameters
+    ----------
+    item (dict): Corpus record to insert
+    Returns
+    -------
+    None
+    """
+    #Resource creation
     dynamodb = boto3.resource('dynamodb',
         region_name='us-east-1',
         aws_access_key_id=os.environ['DYNAMODB_KEY'],
         aws_secret_access_key=os.environ['DYNAMODB_SECRET'])
     id = str(uuid.uuid1())
     dt = str(datetime.utcnow())
-
+    #DynamoDB put_item operation
     table = dynamodb.Table('senti-corpus')
     response = table.put_item(
        Item={
@@ -118,6 +190,14 @@ def insert(item):
     return response
 
 def item_count():
+    """
+    Retrieves the approximate record count from a DynamoDB table.
+    Parameters
+    ----------
+    Returns
+    -------
+    int
+    """
     dynamodb = boto3.resource('dynamodb',
         region_name='us-east-1',
         aws_access_key_id=os.environ['DYNAMODB_KEY'],
@@ -125,15 +205,40 @@ def item_count():
     table = dynamodb.Table('senti-corpus')
     return table.item_count
 
+#Loads pre-trained classifier from .pickle file
 saved_classifier = open("./app/naivebayes.pickle", "rb")
 classifier = pickle.load(saved_classifier)
 saved_classifier.close()
-print("Sentiment Classifier Loaded")
+logging.info("Sentiment Classifier Loaded")
 
 @api.route("/corpus")
 class Corpus(Resource):
-
+    """
+    A class that represents the /corpus API endpoint.
+    ...
+    Attributes
+    ----------
+    Methods
+    -------
+    options(self):
+        Returns a response with HTTP headers to a pre-flight request
+        to allow for Cross-Origin Resource Sharing (CORS).
+    post(self):
+        Recevies a payload (phrase-sentiment submission) from a client
+        and inserts it into the DynamoDB Table.
+    get(self):
+        Returns a response containing the record count of the DynamoDB Table.
+    """
     def options(self):
+        """
+        Returns a response with HTTP headers to a pre-flight request
+        to allow for Cross-Origin Resource Sharing (CORS)
+        Parameters
+        ----------
+        Returns
+        -------
+        JSON Object
+        """
         response = make_response()
         response.headers.add("Access-Control-Allow-Origin", "*")
         response.headers.add("Access-Control-Allow-Headers", "*")
@@ -141,6 +246,15 @@ class Corpus(Resource):
         return response
 
     def post(self):
+        """
+        Recevies a payload (phrase-sentiment submission) from a client
+        and inserts it into the DynamoDB Table.
+        Parameters
+        ----------
+        Returns
+        -------
+        JSON Object
+        """
         try:
             item = request.get_json()
             insert(item)
@@ -156,6 +270,14 @@ class Corpus(Resource):
             return jsonify({"statusCode": 500, "status": "Error", "error": str(error)})
 
     def get(self):
+        """
+        Returns a response containing the record count of the DynamoDB Table.
+        Parameters
+        ----------
+        Returns
+        -------
+        JSON Object
+        """
         try:
             count = item_count()
             response = jsonify(
@@ -172,7 +294,30 @@ class Corpus(Resource):
 
 @api.route("/vader/<string:text>")
 class Vader(Resource):
+    """
+    A class that represents the /vader API endpoint.
+    ...
+    Attributes
+    ----------
+    Methods
+    -------
+    options(self, text):
+        Returns a response with HTTP headers to a pre-flight request
+        to allow for Cross-Origin Resource Sharing (CORS).
+    get(self, text):
+        Returns a response containing the classification and polarity scores
+        for a particular text string using the VADER algorithm.
+    """
     def options(self, text):
+        """
+        Returns a response with HTTP headers to a pre-flight request
+        to allow for Cross-Origin Resource Sharing (CORS).
+        Parameters
+        ----------
+        Returns
+        -------
+        JSON Object
+        """
         response = make_response()
         response.headers.add("Access-Control-Allow-Origin", "*")
         response.headers.add("Access-Control-Allow-Headers", "*")
@@ -181,8 +326,17 @@ class Vader(Resource):
 
     @api.expect(get_request)
     def get(self, text):
+        """
+        Returns a response containing the classification and polarity scores
+        for a particular text string using the VADER algorithm.
+        Parameters
+        ----------
+        Returns
+        -------
+        JSON Object
+        """
         try:
-            classification = vader(text, classifier)
+            classification = vader(text)
             response = jsonify(
                 {
                     "statusCode": 200,
@@ -197,7 +351,30 @@ class Vader(Resource):
 
 @api.route("/bayes/<string:text>")
 class NaiveBayes(Resource):
+    """
+    A class that represents the /bayes API endpoint.
+    ...
+    Attributes
+    ----------
+    Methods
+    -------
+    options(self, text):
+        Returns a response with HTTP headers to a pre-flight request
+        to allow for Cross-Origin Resource Sharing (CORS).
+    get(self, text):
+        Returns a response containing the sentiment classification
+        for a particular text string using a pre-trained naive bayes classifier.
+    """
     def options(self, text):
+        """
+        Returns a response with HTTP headers to a pre-flight request
+        to allow for Cross-Origin Resource Sharing (CORS).
+        Parameters
+        ----------
+        Returns
+        -------
+        JSON Object
+        """
         response = make_response()
         response.headers.add("Access-Control-Allow-Origin", "*")
         response.headers.add("Access-Control-Allow-Headers", "*")
@@ -206,6 +383,15 @@ class NaiveBayes(Resource):
 
     @api.expect(get_request)
     def get(self, text):
+        """
+        Returns a response containing the sentiment classification
+        for a particular text string using a pre-trained naive bayes classifier.
+        Parameters
+        ----------
+        Returns
+        -------
+        JSON Object
+        """
         try:
             result = naive_bayes(text, classifier)
             classification = {
@@ -225,7 +411,31 @@ class NaiveBayes(Resource):
 
 @api.route("/bulk")
 class Bulk(Resource):
+    """
+    A class that represents the /bulk API endpoint.
+    ...
+    Attributes
+    ----------
+    Methods
+    -------
+    options(self):
+        Returns a response with HTTP headers to a pre-flight request
+        to allow for Cross-Origin Resource Sharing (CORS).
+    put(self):
+        Receives an array of documents and analyses each document's contents
+        using the VADER algorithm, returning the classification and polarity scores
+        for each requested document.
+    """
     def options(self):
+        """
+        Returns a response with HTTP headers to a pre-flight request
+        to allow for Cross-Origin Resource Sharing (CORS).
+        Parameters
+        ----------
+        Returns
+        -------
+        JSON Object
+        """
         response = make_response()
         response.headers.add("Access-Control-Allow-Origin", "*")
         response.headers.add("Access-Control-Allow-Headers", "*")
@@ -233,9 +443,20 @@ class Bulk(Resource):
         return response
 
     def put(self):
+        """
+        Receives an array of documents and analyses each document's contents
+        using the VADER algorithm, returning the classification and polarity scores
+        for each requested document.
+        Parameters
+        ----------
+        Returns
+        -------
+        JSON Object
+        """
         try:
             payload = request.get_json()
             classification = []
+            # Processing of each document
             for document in payload['documents']:
                 classification.append(vader(document['text'], classifier))
             response = jsonify(
